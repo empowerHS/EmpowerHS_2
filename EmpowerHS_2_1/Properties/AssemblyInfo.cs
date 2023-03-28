e00fd6debc7cc3a151cf05e2e6a48a5efa98cd02
Harald Schwarz <harald.schwarz@global.ntt>
2023-03-28 15:25:00 +0200

    Add project files.

---
+++ EmpowerHS_2_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerHS_2_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerHS_2_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("A2BD9C39-29AB-496F-978E-EA6E72FF4486")]
+[assembly: AssemblyVersion("1.0.0.0")]
